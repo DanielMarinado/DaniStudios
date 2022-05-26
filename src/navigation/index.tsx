c12a7e278dd87a498ea85c94d322ab4ebcc8d23c
Daniel Marinado <daniel.marinado@gmail.com>
2022-05-26 00:40:34 -0400

    Agrega vision Camera config

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,11 +2,23 @@ import { View, Text } from 'react-native'
 import React, { createContext, useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import StackNavigator from './StackNavigator';
+import { Camera } from 'react-native-vision-camera';
+
 
 export const PeliculasContext = createContext({});
 
 
 const Index = () => {
+
+  const initCamera = async() =>{
+    const newCameraPermission = await Camera.requestCameraPermission()
+    const newMicrophonePermission = await Camera.requestMicrophonePermission()
+  }
+
+  useEffect(() => {
+    initCamera()
+  }, [])
+  
 
   const [peliculas, setPeliculas] = useState([]);
 
